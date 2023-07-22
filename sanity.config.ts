208cf01981b9389df6166105c6fb5d962c128181
Songa Musiwa <victor.musiwa@gmail.com>
2023-07-23 07:18:52 +0900

    fresh

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,15 +2,16 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {markdownSchema} from 'sanity-plugin-markdown'
 
 export default defineConfig({
   name: 'default',
   title: 'digitize-afrika',
 
-  projectId: 'xth1k0s5',
+  projectId: 'wtjm7oz0',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), markdownSchema()],
 
   schema: {
     types: schemaTypes,
